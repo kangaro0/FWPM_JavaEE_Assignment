b03a483f12306ffef59f039ea14f7cc59ac15b69
kangaro0 <johannes_herbert@yahoo.de>
2018-06-06 10:26:48 +0200

    began sql apis

--- src/db/Items.java
+++ src/db/Items.java
@@ -2,6 +2,7 @@ package db;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import models.Item;
 
@@ -16,23 +17,65 @@ public class Items {
 	
 	public ArrayList<Item> GetAll(){
 		
-		establish();
+		connect();
 		
-		PreparedStatement stmt = _dbConnection.GetConnection().prepareStatement( "SELECT * FROM item" );
-		ResultSet result = stmt.executeQuery();
+		ArrayList<Item> items = new ArrayList<Item>();
 		
-		while( result.next() ){
-			Item current = new Item( result.getInt( 1 ), result.getString( 2 ), result.getFloat( 3 ), result.getInt( 4 ), result.getInt( 5 ), result.getInt( 6 ), result.getInt( 7 ) );
-			_items.add( current );
+		try {
+		
+			PreparedStatement stmt = _dbConnection.GetConnection().prepareStatement( "SELECT * FROM item" );
+			ResultSet result = stmt.executeQuery();
+			
+			while( result.next() ){
+				Item current = new Item( result.getInt( 1 ), result.getString( 2 ), result.getFloat( 3 ), result.getInt( 4 ), result.getInt( 5 ), result.getInt( 6 ), result.getInt( 7 ) );
+				items.add( current );
+			}
+			
+		} catch( SQLException sqle ){
+			sqle.printStackTrace();
 		}
+		
+		disconnect();
+		
+		return items;
 	}
 	
-	private void establish(){
+	public Item GetById( int id ){
+		for( Item i : _items ){
+			if( i.getId() == id )
+				return i;
+		}
+		return null;
+	}
+	
+	public void UpdateItem( Item item ){
+		
+		connect();
+		
+		try {
+			
+			PreparedStatement stmt = _dbConnection.GetConnection().prepareStatement( "UPDATE" );
+		} catch( SQLException sqle ){
+			sqle.printStackTrace();
+		}
+		
+		disconnect();
+	}
+	
+	
+	private void connect(){
 		if( _dbConnection.IsConnected() )
 			return;
 		
 		_dbConnection.Connect();
 	}
 	
-	private void 
+	private void disconnect(){
+		if( _dbConnection.IsConnected() )
+			_dbConnection.Disconnect();
+	}
+	
+	private void initialize(){
+		_items = GetAll();
+	}
 }
